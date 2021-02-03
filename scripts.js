505f3bd9540f2385ab09c5d48d394a46f5e380f0
Jhonny Lima <jlima@Jhonnys-MacBook-Pro.local>
2021-02-03 20:25:56 -0300

    finished js implementation

--- scripts.js
+++ scripts.js
@@ -10,42 +10,53 @@ const Modal = {
   }
 }
 
-const transactions = [
-  {
-    id: 1,
-    description: 'Luz',
-    amount: -50000,
-    date: '23/01/2021'
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
   },
-  {
-    id: 2,
-    description: 'Website',
-    amount: 500000,
-    date: '23/01/2021'
-  },
-  {
-    id: 3,
-    description: 'Internet',
-    amount: -20000,
-    date: '23/01/2021'
-  },
-  {
-    id: 4,
-    description: 'App',
-    amount: 200000,
-    date: '23/01/2021'
+
+  set(transactions) {
+    localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
   }
-]
+}
 
 const Transaction = {
+  all: Storage.get(),
+
+  add(transaction) {
+    Transaction.all.push(transaction)
+
+    App.reload()
+  },
+
+  remove(index) {
+    Transaction.all.splice(index, 1)
+
+    App.reload()
+  },
+
   incomes() {
-
+    let income = 0;
+    Transaction.all.forEach( transaction => {
+      if( transaction.amount > 0 ) {
+        income += transaction.amount;
+      }
+    })
+    return income;
   },
+
   expenses() {
-
+    let expense = 0;
+    Transaction.all.forEach( transaction => {
+      if( transaction.amount < 0 ) {
+        expense += transaction.amount;
+      }
+    })
+    return expense;
   },
-  total() {
 
+  total() {
+    return this.incomes() + this.expenses();
   }
 }
 
@@ -54,12 +65,13 @@ const DOM = {
 
   addTransaction(transaction, index) {
     const tr = document.createElement('tr')
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+    tr.dataset.index = index
 
     DOM.transactionsContainer.appendChild(tr)
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const cssClass = transaction.amount > 0 ? "income" : "expense"
 
     const amount = Utils.formatCurrency(transaction.amount)
@@ -69,11 +81,84 @@ const DOM = {
       <td class=${cssClass}>${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
-        <img src="./assets/minus.svg" alt="Remover transação">
+        <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
       </td>
     `
 
     return html
+  },
+
+  updateBalance() {
+    document
+      .getElementById('income-display')
+      .innerHTML = Utils.formatCurrency(Transaction.incomes())
+    document
+      .getElementById('expense-display')
+      .innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document
+      .getElementById('total-display')
+      .innerHTML = Utils.formatCurrency(Transaction.total())
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = ""
+  }
+}
+
+const Form = {
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value
+    }
+  },
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues()
+    
+    if( description.trim() === "" ||
+        amount.trim() === "" ||
+        date.trim() === "" ) {
+          throw new Error("Por favor, preencha todos os campos!")
+        }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues()
+
+    amount = Utils.formatAmount(amount)
+    date = Utils.formatDate(date)
+
+    return {
+      description,
+      amount,
+      date
+    }
+  },
+
+  clearFields() {
+    Form.description.value = ""
+    Form.amount.value = ""
+    Form.date.value = ""
+  },
+
+  submit(event) {
+    event.preventDefault()
+
+    try {
+      Form.validateFields()
+      const transaction = Form.formatValues()
+      Transaction.add(transaction)
+      Form.clearFields()
+      Modal.toggle()
+    } catch ( error ) {
+      alert(error.message)
+    }
   }
 }
 
@@ -89,9 +174,37 @@ const Utils = {
     })
 
     return signal + value
+  },
+
+  formatAmount(value) {
+    value = Number(value) * 100
+    // value = Number(value.replace(/\,\./g,"")) * 100
+
+    return value
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split("-")
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+  },
+}
+
+const App = {
+  init() {
+    // Transaction.all.forEach( (transaction, index) => {
+    //   DOM.addTransaction(transaction, index)
+    // })
+    Transaction.all.forEach(DOM.addTransaction)
+    
+    DOM.updateBalance()
+
+    Storage.set(Transaction.all)
+  },
+
+  reload() {
+    DOM.clearTransactions()
+    App.init()
   }
 }
 
-transactions.forEach(function(transaction) {
-  DOM.addTransaction(transaction)
-})
+App.init()
