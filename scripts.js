9d38382bfe60c9a8042d07994aae8da36e21f76c
Jhonny Lima <jlima@Jhonnys-MacBook-Pro.local>
2021-02-05 18:20:37 -0300

    fixed bug on formatAmount

--- scripts.js
+++ scripts.js
@@ -177,10 +177,8 @@ const Utils = {
   },
 
   formatAmount(value) {
-    value = Number(value) * 100
-    // value = Number(value.replace(/\,\./g,"")) * 100
-
-    return value
+    value = value * 100
+    return Math.round(value)
   },
 
   formatDate(date) {
